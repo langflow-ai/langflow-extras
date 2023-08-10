635e12ed05b4074c40dd713d62c6b3c7344405a2
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-10 17:14:46 -0300

    🔧 chore(pyproject.toml): update langflow_extras script name to langflow in pyproject.toml 🔧 chore(main.py): refactor run_langflow_extras function to main function and update imports to use existing_app instead of app

--- src/langflow_extras/main.py
+++ src/langflow_extras/main.py
@@ -1,16 +1,22 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv, find_dotenv
+from langflow.__main__ import (
+    app as existing_app,
+    # main as existing_main,
+)  # Import the original app
 
 
-def run_langflow_extras():
-    from langflow.__main__ import main
-
+@existing_app.callback(invoke_without_command=True)
+def run():
     components_path = Path(__file__).parent / "components"
     os.environ["LANGFLOW_COMPONENT_PATH"] = str(components_path)
     load_dotenv(find_dotenv())
-    main()
+
+
+def main():
+    existing_app()  # Run the existing app
 
 
 if __name__ == "__main__":
-    run_langflow_extras()
+    main()
