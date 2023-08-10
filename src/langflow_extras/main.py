f8fe2be7818ae38babac0c7c6169e8985674005a
Gabriel Luiz Freitas Almeida <gabriel@logspace.ai>
2023-08-10 17:37:15 -0300

    🔧 chore(main.py): change environment variable name from LANGFLOW_COMPONENT_PATH to LANGFLOW_COMPONENTS_PATH to improve clarity and consistency 🔧 chore(main.py): update LANGFLOW_COMPONENTS_PATH value to be a JSON string containing the path to the components directory

--- src/langflow_extras/main.py
+++ src/langflow_extras/main.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -10,7 +11,7 @@ from langflow.__main__ import (
 @existing_app.callback(invoke_without_command=True)
 def run():
     components_path = Path(__file__).parent / "components"
-    os.environ["LANGFLOW_COMPONENT_PATH"] = str(components_path)
+    os.environ["LANGFLOW_COMPONENTS_PATH"] = json.dumps([str(components_path)])
     load_dotenv(find_dotenv())
 
 
